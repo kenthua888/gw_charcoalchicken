35370a54137a542f9a173a4da8b81b1b012850ca
Kent <khua0031@student.monash.edu>
2025-03-08 17:40:33 +1100

    style(css): Included title background color, readjust div element. Added new row with 3 new images. Also included item banner on each image for better design

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -11,24 +11,29 @@ import greekPack from "../assets/img/greek_pack.png";
 import greekSalad from "../assets/img/greek_salad.png";
 import chickenSchnitzel from "../assets/img/chicken_schnitzel_roll.png";
 import chickenRoll from "../assets/img/chicken_roll.png";
+import friedRice from "../assets/img/fried_rice.png";
+import lambSouvlaki from "../assets/img/lamb_souvlaki.png";
+import wholeChicken from "../assets/img/whole_chicken.png";
 
 const images = [
     { src: halfChicken, name: "Half Chicken" },
+    { src: wholeChicken, name: "Whole Chicken" },
     { src: quarterChicken, name: "Quarter Chicken" },
     { src: chickenSchnitzel, name: "Chicken Schnitzel Roll" },
     { src: hsp, name: "Halal Snack Pack" },
+    { src: lambSouvlaki, name: "Lamb Souvlaki" },
     { src: chickenRoll, name: "Chicken Roll" },
     { src: greekPack, name: "Greek Pack" },
     { src: greekSalad, name: "Greek Salad" },
     { src: singaporeNoodles, name: "Singapore Noodles" },
-    { src: kwayTeow, name: "Kway Teow" }
+    { src: kwayTeow, name: "Kway Teow" },
+    { src: friedRice, name: "Special Fried Rice" }
 ];
 
 const Gallery = () => {
     return (
         <GalleryContainer>
-            <h1>Photo Gallery</h1>
-            <p>Check out our mouthwatering dishes...</p>
+            <h1>Gallery</h1>
 
             <div className="container">
                 <div className="row text-center">
@@ -36,8 +41,8 @@ const Gallery = () => {
                         <div key={index} className="col-4">
                             <ImageWrapper>
                                 <img src={item.src} alt={item.name} />
+                                <OverlayText>{item.name}</OverlayText> {/* Show dish name instead of generic text */}
                             </ImageWrapper>
-                            <p>{item.name}</p> {/* Show dish name instead of generic text */}
                         </div>
                     ))}
                 </div>
@@ -47,22 +52,43 @@ const Gallery = () => {
 };
 
 const GalleryContainer = styled.div`
-    max-width: 1200px;
     margin: 0 auto;
-    padding: 2rem;
+
+    h1 {
+        background-color: #F3F8E5;
+        color: #7fbc42;
+        padding: 2rem 2rem;
+    }
+
 `;
 
 const ImageWrapper = styled.div`
     width: 100%;
-    height: 200px;  /* Set a fixed height */
+    height: 250px;  /* Set a fixed height */
+
     overflow: hidden; /* Hide any overflow */
+    position: relative;
+    display: inline-block;
     
     img {
         width: 100%;
         height: 100%;
+        display: block;
         object-fit: cover; /* Crop the image to fit without distortion */
         border-radius: 10px; /* Optional: Add rounded corners */
     }
 `;
+const OverlayText = styled.h5`
+    position: absolute;
+    bottom: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+    background: rgba(53, 52, 52, 0.6); /* Optional: Adds background for readability */
+    color: white;
+    padding: 5px !important;
+    border-radius: 5px;
+    margin: 0;
+    width: 100%
 
+`;
 export default Gallery;
